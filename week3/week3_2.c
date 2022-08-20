8de668530baa9d242879ffbc4fa145678c139bf8
tometoblush <86197074+ththanan@users.noreply.github.com>
2022-08-20 14:45:44 +0700

    Add files via upload

---
+++ week3/week3_2.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+    int num1, num2;
+    scanf("%d", &num);
+    num1 = num;
+    num2 = 2;
+    for (int i = 1; i <= num; i++)
+    {
+        for (int s = 1; s < i; s++)
+        {
+            printf(" ");
+        }
+        for (int j = 1; j <= num1; j++)
+        {
+            printf("*");
+        }
+        for (int j = 1; j < num1; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+        num1--;
+    }
+    for (int i = num - 1; i >= 1; i--)
+    {
+        for (int s = 1; s < i; s++)
+        {
+            printf(" ");
+        }
+        for (int j = 1; j <= num2; j++)
+        {
+            printf("*");
+        }
+        for (int j = 1; j < num2; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+        num2++;
+    }
+    return 0;
+}
