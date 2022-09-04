1dc91fea9f939e365b14c591fd631da68f396c60
tometoblush <86197074+ththanan@users.noreply.github.com>
2022-09-04 13:49:40 +0700

    Add files via upload

---
+++ week5/week5_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char sentence[100];
+    scanf("%s", sentence);
+    for(int i = strlen(sentence); i > 0; i--)
+    {
+        for(int j = 0; j < i; j++)
+        {
+            printf("%c", sentence[j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
---
+++ week5/week5_2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char sentence[100];
+    char ans[100][100];
+    scanf("%s", sentence);
+    for(int i = 0; i < strlen(sentence); i++)
+    {
+        for(int j = 0; j <= i; j++)
+        {
+            ans[i][j] = sentence[j];
+        }
+    }
+    for(int i = strlen(sentence) - 1; i >= 0; i--)
+    {
+        for(int j = 0; j <= i ; j++)
+        {
+            printf("%c", ans[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
