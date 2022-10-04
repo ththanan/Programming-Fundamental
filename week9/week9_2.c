ce04b511dae6621d54a49a820e5140a20eb7d7af
tometoblush <86197074+ththanan@users.noreply.github.com>
2022-10-04 08:35:14 +0700

    Update week9_2.c

--- week9/week9_2.c
+++ week9/week9_2.c
@@ -1,46 +1,35 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-    int n1, n2, n3, n=0;
-    char abc[3];
-    scanf("%d %d %d", &n1, &n2, &n3);
-    scanf("%s", abc);
-    if((n1 > 0 && n1 < 100) && (n2 > 0 && n2 < 100) && (n3 > 0 && n3 < 100))
+    int a[5], b[5];
+    int all_num[10];
+    for(int i = 0; i < 10; i++)
     {
-        if(n3 < n2)
-        {
-            n = n2;
-            n2 = n3;
-            n3 = n;
-        }
-        if(n2 < n1)
-        {
-            n = n1;
-            n1 = n2;
-            n2 = n;
-        }
-        if(n3 < n2)
-        {
-            n = n2;
-            n2 = n3;
-            n3 = n;
-        }
-        for(int i = 0 ; i <= 2; i++)
-        {
-            if(abc[i] == 'A')
-            {
-                abc[i] = n1;
-            }
-            else if(abc[i] == 'B')
-            {
-                abc[i] = n2;
-            }
-            else if(abc[i] == 'C')
-            {
-                abc[i] = n3;
-            }
-        }
-        printf("%d %d %d", abc[0], abc[1], abc[2]);
+        all_num[i] = 0;
     }
-    return 0;
+    for(int i = 0; i < 5; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    for(int i = 0; i < 5; i++)
+    {
+        all_num[a[i]]++;
+    }
+    for(int i = 0; i < 5; i++)
+    {
+        scanf("%d", &b[i]);
+    }
+    for(int i = 0; i < 5; i++)
+    {
+        all_num[b[i]]++;
+    }
+
+    for(int i = 0; i < 10; i++)
+    {
+        if(all_num[i] > 1)
+        {
+            printf("%d",i);
+        }
+    }
+    return 0; 
 }
